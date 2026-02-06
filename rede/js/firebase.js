c6204714e20b52e30d8c878b6915710fbd2b64eb
lojinhadosautistas <lojinhadosautistas@gmail.com>
2026-02-05 23:25:47 -0300

    Create firebase.js

---
+++ rede/js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "sistema-rede.firebaseapp.com",
+  projectId: "sistema-rede",
+  storageBucket: "sistema-rede.appspot.com",
+  messagingSenderId: "930399212687",
+  appId: "1:930399212687:web:a89c0d73be4b9fbd488cfa"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
