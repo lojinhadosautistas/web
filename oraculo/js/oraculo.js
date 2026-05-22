623dc26184fc7e4cfb87f2a7243773e9353c9f65
lojinhadosautistas <lojinhadosautistas@gmail.com>
2026-05-22 20:21:10 -0300

    Update oraculo.js

--- oraculo/js/oraculo.js
+++ oraculo/js/oraculo.js
@@ -1,1 +1,19 @@
+function obterResposta(camada){
 
+  const bibliotecas = {
+    1: respostasCamada1,
+    2: respostasCamada2,
+    3: respostasCamada3,
+    4: respostasCamada4,
+    5: respostasCamada5,
+    6: respostasCamada6,
+    7: respostasCamada7
+  };
+
+  const lista = bibliotecas[camada];
+
+  return lista[
+    Math.floor(Math.random()*lista.length)
+  ];
+
+}
