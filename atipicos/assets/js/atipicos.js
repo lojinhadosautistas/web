585737007d070b08ec939a9a374107b57fc06e78
lojinhadosautistas <lojinhadosautistas@gmail.com>
2026-06-04 16:48:11 -0300

    Create atipicos.js

---
+++ atipicos/assets/js/atipicos.js
@@ -0,0 +1,30 @@
+
+<!-- =========================================================
+     GLOBAL AUDIO PLAYER
+========================================================= -->
+
+<div class="audio-player">
+
+    <div id="playerIcon" class="player-icon">
+        ♪
+    </div>
+
+    <div class="player-info">
+
+        <div id="playerTitle">
+            Escolha um abraço musical.
+        </div>
+
+    </div>
+
+    <button id="prevBtn" class="player-control">
+        ◀
+    </button>
+
+    <audio id="audioControl" controls></audio>
+
+    <button id="nextBtn" class="player-control">
+        ▶
+    </button>
+
+</div>
